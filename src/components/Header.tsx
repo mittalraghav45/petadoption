8650b46f737f832c5121c2a8d3c92e6fff23a1d2
Raghav Mittal <mittalraghav45@gmail.com>
2025-05-08 22:54:09 +0100

    deploy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export function Header() {
           <img src="/img/PetFinderLogo.png" width="80" height={80}></img>
         </Navbar.Brand>
         <Nav className='d-flex justify-conntent-center w-100'>
-            <h1 className="text-center text-white">Help them find tgeir new hikes</h1>
+            <h1 className="text-center text-white">Help them find their new fur-homes</h1>
         </Nav>
       </Container>
     </Navbar>
